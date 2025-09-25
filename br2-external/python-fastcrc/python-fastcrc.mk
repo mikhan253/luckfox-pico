a71acf2c81f09757c0ce83ec8c58847366ca1ccb
mikhan253 <miklautsch25@gmail.com>
2025-09-25 21:05:45 +0200

    external package

---
+++ br2-external/python-fastcrc/python-fastcrc.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-fastcrc
+#
+################################################################################
+
+PYTHON_FASTCRC_VERSION = 0.3.2   # -> aktuelle Version auf PyPI prüfen
+PYTHON_FASTCRC_SOURCE = fastcrc-$(PYTHON_FASTCRC_VERSION).tar.gz
+PYTHON_FASTCRC_SITE = https://files.pythonhosted.org/packages/source/f/fastcrc
+PYTHON_FASTCRC_SETUP_TYPE = setuptools
+PYTHON_FASTCRC_LICENSE = MIT
+PYTHON_FASTCRC_LICENSE_FILES = LICENSE
+
+$(eval $(python-package))
