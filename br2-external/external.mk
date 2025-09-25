116a9a478fcd466bc74530efdf22bad69db6f890
mikhan253 <miklautsch25@gmail.com>
2025-09-25 21:54:09 +0200

    fastcrc

---
+++ br2-external/external.mk
@@ -0,0 +1,1 @@
+include $(sort $(wildcard $(BR2_EXTERNAL_MIK_PATH)/package/*/*.mk))
--- br2-external/package/python-fastcrc/python-fastcrac.mk
+++ br2-external/package/python-fastcrc/python-fastcrac.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-PYTHON_FASTCRC_VERSION = 0.3.2   # -> aktuelle Version auf PyPI prüfen
+PYTHON_FASTCRC_VERSION = 0.3.2
 PYTHON_FASTCRC_SOURCE = fastcrc-$(PYTHON_FASTCRC_VERSION).tar.gz
 PYTHON_FASTCRC_SITE = https://files.pythonhosted.org/packages/source/f/fastcrc
-PYTHON_FASTCRC_SETUP_TYPE = setuptools
+PYTHON_FASTCRC_SETUP_TYPE = pep517
+PYTHON_FASTCRC_DEPENDENCIES = host-python-maturin
 PYTHON_FASTCRC_LICENSE = MIT
 PYTHON_FASTCRC_LICENSE_FILES = LICENSE
 
