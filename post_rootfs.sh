1890f4bb08b3aa43c103fca2e580cb2dd2b29e06
mikhan253 <miklautsch25@gmail.com>
2025-10-01 18:46:43 +0200

    fix

--- post_rootfs.sh
+++ post_rootfs.sh
@@ -4,7 +4,7 @@ echo "Post-processing root filesystem at $1"
 echo ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 rmdir $1/var/lib/iwd
-ln -s /run/wifi_config $1/var/lib/iwd
+ln -s /run/config_wifi $1/var/lib/iwd
 rm -f $1/etc/dropbear
 ln -s /mnt/userdata/config/ssh $1/etc/dropbear
 rm $1/etc/os-release
