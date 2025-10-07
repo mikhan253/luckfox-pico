716716580b5a743fb10fa3312c974d4cc4396c17
mikhan253 <miklautsch25@gmail.com>
2025-10-07 20:14:40 +0200

    bjoern hinzufuegen

--- br2-external/package/python-bjoern/python-bjoern.mk
+++ br2-external/package/python-bjoern/python-bjoern.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #
-# python-fastcrc
+# python-bjoern
 #
 ################################################################################
 
@@ -8,7 +8,19 @@ PYTHON_BJOERN_VERSION = 3.2.2
 PYTHON_BJOERN_SOURCE = bjoern-$(PYTHON_BJOERN_VERSION).tar.gz
 PYTHON_BJOERN_SITE = https://files.pythonhosted.org/packages/source/b/bjoern
 PYTHON_BJOERN_SETUP_TYPE = setuptools
-PYTHON_BJOERN_LICENSE = MIT
+PYTHON_BJOERN_LICENSE = BSD-2-Clause
 PYTHON_BJOERN_LICENSE_FILES = LICENSE
+PYTHON_BJOERN_DEPENDENCIES = libev
+
+define PYTHON_BJOERN_FIX_SETUP_PY
+    $(SED) 's|/usr/include/libev|$(STAGING_DIR)/usr/include|g' $(@D)/setup.py
+    $(SED) 's|/opt/local/include|$(STAGING_DIR)/usr/include|g' $(@D)/setup.py
+    $(SED) 's|/opt/homebrew/include|$(STAGING_DIR)/usr/include|g' $(@D)/setup.py
+    $(SED) 's|/usr/local/include|$(STAGING_DIR)/usr/include|g' $(@D)/setup.py
+	
+    $(SED) 's|library_dirs *= *\[[^]]*\]|library_dirs = ["$(STAGING_DIR)/usr/lib"]|' $(@D)/setup.py
+endef
+
+PYTHON_BJOERN_POST_PATCH_HOOKS += PYTHON_BJOERN_FIX_SETUP_PY
 
 $(eval $(python-package))
